f64fa0657b7fe03d1102f82dd46089f58847e9aa
Hyeonsoo Kim <hyeonsoo1592@gmail.com>
2020-04-21 14:45:30 +0900

    [6] PA02 : Write Argument Routine

--- Programming_Assignments/60_201424442_김현수_PA2/PA02.cpp
+++ Programming_Assignments/60_201424442_김현수_PA2/PA02.cpp
@@ -5,8 +5,55 @@
  * 2020.05.05
  * ===========================================================================
  */
-
-/* 
- * 
+ 
 #include <iostream>
 #include <fstream>
+#include <functional>
+#include <cerrno>
+#include <stdexcept>
+#include <cstdio>
+#include <string>
+using namespace std;
+
+/* 
+ * Driver Routine Object
+ */
+
+class functionality {
+
+		/* 
+		 * Private Data for Driver Routine
+		 *  int errstate : error state number
+		 *  str progname : program name
+		 */
+	private:
+		/* 
+		 * errstate : Error number of system.
+		 *   Normaly, errstate is error number in posix system. but there are 2 special cases
+		 *     -1 : Argument Error 
+		 *     -2 : An Exception is caught
+		 */ 
+		int errstate;
+		string progname;
+	public:
+		functionality(const char* _name):errstate(0), progname(string(_name)){}
+		function<const int(const int, const string&)> printerror = [&](const int _err, const string& msg) ->const int{
+				errstate = _err;
+				cout << "[X] ERROR! " << msg << endl;
+				if ( errstate == -1 ) cout << "\t Please check usage above."; 
+				else if (errstate == -2) cout << "\t Runtime Exception occured!";
+				else perror("\t(Message) ");
+				cout << " this program terminated!"<<endl;
+				return errstate;
+			};	
+};
+
+int main(int argc, const char **argv) {
+		functionality Driver(argv[0]);
+		if(argc !=2 ){
+			return Driver.printerror(-1, (string("Usage : ")+string(argv[0])+string(" [only 1 filename]")));
+			}
+		else {
+			}
+		return 0;
+}
